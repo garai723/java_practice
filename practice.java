28e9c06830b92d069a93df095e6d050dcce9fea1
garai <garai@crooz.co.jp>
2015-12-03 23:11:01 +0900

    hand in assinment

---
+++ practice.java
@@ -0,0 +1,19 @@
+﻿ /**
+ テキスト1章　練習問題
+ */
+ 
+ public class Plactice { 
+  public static void main(String[]args){
+	int a = 3 ;
+	int b = 5 ;
+	int c = a*b;
+	System.out.println ("縦幅3横幅5の長方形の面積は、" +a*b);
+
+	boolean bln = true ;
+	char name = '駆' ;
+	double d = 3.14 ;
+	long e = 314159265853979L ;
+	String attack = "ミナトの攻撃！敵に15ポイントのダメージを与えた。" ;
+
+  }
+ }
