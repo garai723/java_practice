76d02f3ee6f1c8d25347a1cd55f64546a6d3b019
garai <garai@crooz.co.jp>
2015-12-04 20:37:18 +0900

    hand in assinment

---
+++ FourI.java
@@ -0,0 +1,8 @@
+﻿public class FourI{
+ public static void main(String[]args){
+	int[] moneyList = {121902, 8302, 55100};
+	for(int money : moneyList){
+		System.out.println(money);
+  }
+ }
+}
---
+++ FourII.java
@@ -0,0 +1,13 @@
+﻿public class Main{
+ public static void main(String[]args){
+	int[] numbers = {3, 4, 9};
+	System.out.println("1桁の数字を入力してください");
+	int input = new java.util.Scanner(System.in).nextInt();
+	
+	for(int number : numbers){
+		if(number == input){
+			System.out.println("当たり");
+   }
+  }
+ }
+}
